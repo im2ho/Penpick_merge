e0426348e53803235cfbe05035a515111f123643
im2ho <84431276+im2ho@users.noreply.github.com>
2024-02-13 20:59:47 +0900

    ahffk

--- client/src/components/WriteQusetion2.js
+++ client/src/components/WriteQusetion2.js
@@ -5,8 +5,15 @@ import '../css/WriteQuestion.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function WriteQuestion() {
-  
-  const [nickname, setNickname] = useState("");
+
+  const [question, setQuestion] = useState({
+    nickname: "",
+    questionContent:"",
+    questionTitle:"",
+    writtenDate: new Date(),
+  });
+
+  // const [nickname, setNickname] = useState("");
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
@@ -16,7 +23,8 @@ export default function WriteQuestion() {
         const res = await axios.get("http://localhost:8282/userdata", {
           withCredentials: true,
         });
-        setNickname(res.data.nickname);
+        setQuestion({...question, nickname:res.data.nickname});
+        console.log(question);
       } catch (err) {
         console.error("세션 데이터 불러오기 실패", err);
       }
@@ -30,10 +38,7 @@ const handleSubmit = async (e) => {
     try {
 
       const res = await axios.post("http://localhost:8282/QnA/write-question", {
-        nickname: nickname,
-        questionTitle: title,
-        questionContent: content,
-        writtenDate: new Date(), 
+        question
       });
       console.log(res.data);
       alert("문의글이 등록되었습니다.");
@@ -57,7 +62,7 @@ const handleSubmit = async (e) => {
           <input
             type="text"
             value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            onChange={(e) => setTitle(question.questionTitle, e.target.value)}
             required
           />
         </div>
@@ -65,7 +70,7 @@ const handleSubmit = async (e) => {
           <label>내용</label>
           <textarea
             value={content}
-            onChange={(e) => setContent(e.target.value)}
+            onChange={(e) => setContent(question.questionContent, e.target.value)}
             required
           />
         </div>
